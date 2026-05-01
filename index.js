b3fd0bdff05ae973f56030fc292f280b2c693ace
JonCodingScript <jbenedikt05@gmail.com>
2026-05-01 20:37:46 +0800

    Added primitive value types

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 // First JavaScript code!
 console.log('Hello World');
+
+let name = 'Jon';
+console.log(name);
+
+const interest = '0.3';
+// let interest = '0.5';
+console.log(interest)
+
+// Primitive Types
+// let name = 'Jon' -> String Literal
+let age = 21; // -> Number Literal
+let isTrue = true; // -> Boolean Literal
+let firstName = undefined; // Automatically set to undefined either way
+let selectedColour = 'red'; // Use null when we want to clear the value of a variaible
