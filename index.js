bce73c18725d1bd9368bea7a20de99c9b5be53ce
JonCodingScript <jbenedikt05@gmail.com>
2026-05-01 22:44:30 +0800

    Arrays

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ console.log(interest)
 let age = 21; // -> Number Literal
 let isTrue = true; // -> Boolean Literal
 let firstName = undefined; // Automatically set to undefined either way
-let selectedColour = null; // Use null when we want to clear the value of a variaible
+let colours = null; // Use null when we want to clear the value of a variaible
 
 let person = {
     name: 'Jon',
@@ -35,4 +35,20 @@ let selection = 'name'
 person[selection] = 'Benedikt';
 console.log(person.name);
 
+// Arrays
+let colours = ['red', 'blue', 'green'];
+console.log(colours);
 
+// Adding another element
+colours[3] = 'yellow';
+console.log(colours);
+
+// JS can store different data types in an array
+colours[4] = 1;
+console.log(colours);
+
+// Printing a chosen element
+console.log(colours[1]);
+
+// Properties of an array
+console.log(colours.length);
